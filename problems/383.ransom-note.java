360d52b83c0a3f03719bf09c6c522648c2ff9eb9
CosetteX <xuke324@hotmail.com>
2022-03-25 12:11:05 +0800

    Update 383.ransom-note.java

--- problems/383.ransom-note.java
+++ problems/383.ransom-note.java
@@ -8,7 +8,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         // 借用python的count方法的思想
-        // 
+        // 用一个数组来count magazine里面字母出现的次数
+
+        int[] cnt=new int[26];
+        int n=magazine.length();
+
+        for (int i=0;i<n;i++){
+            int idx=magazine.charAt(i)-'a';
+            cnt[idx]++;
+        }
         
     }
 }
